b3e1f235a6ee7410f59ebb88a779562298b7cea5
Scott Yang <scott.yang@shopify.com>
2022-12-13 08:35:46 -0600

    mockData

--- mockData.js
+++ mockData.js
@@ -1,149 +1,149 @@
 export const mockData = [
   {
     id: 456435643,
-    time: "8:45 AM",
+    time: '8:45 AM',
     data: [
       {
         id: 1,
-        title: "Registration & breakfast",
-        description: "Register and grab some breakfast and coffee.",
-        time: "8:45 AM",
-        speaker: null
-      }
-    ]
+        title: 'Registration & breakfast',
+        description: 'Register and grab some breakfast and coffee.',
+        time: '8:45 AM',
+        speaker: null,
+      },
+    ],
   },
   {
     id: 1234234235,
-    time: "9:30 AM",
+    time: '9:30 AM',
     data: [
       {
         id: 2,
-        title: "Keynote",
+        title: 'Keynote',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "9:30 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '9:30 AM',
         speaker: {
-          name: "Haris Mahmood",
-          role: "Chief Millenial Officer",
-          avatar: "https://api.adorable.io/avatars/285/harismahmood.png"
-        }
-      }
-    ]
+          name: 'Haris Mahmood',
+          role: 'Chief Millenial Officer',
+          avatar: 'https://api.adorable.io/avatars/285/harismahmood.png',
+        },
+      },
+    ],
   },
   {
     id: 56756352,
-    time: "10:15 AM",
+    time: '10:15 AM',
     data: [
       {
         id: 3,
-        title: "Introduction to React Native",
+        title: 'Introduction to React Native',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "10:15 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '10:15 AM',
         speaker: {
-          name: "Ryan Baldwin",
-          role: "Baker of Bytes",
-          avatar: "https://api.adorable.io/avatars/285/ryan.png"
-        }
+          name: 'Ryan Baldwin',
+          role: 'Baker of Bytes',
+          avatar: 'https://api.adorable.io/avatars/285/ryan.png',
+        },
       },
       {
         id: 4,
-        title: "React Native at Scale",
+        title: 'React Native at Scale',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "10:15 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '10:15 AM',
         speaker: {
-          name: "Sergey Gavrilyuk",
-          role: "Hardcore Canadian",
-          avatar: "https://api.adorable.io/avatars/285/sergey.png"
-        }
+          name: 'Sergey Gavrilyuk',
+          role: 'Hardcore Canadian',
+          avatar: 'https://api.adorable.io/avatars/285/sergey.png',
+        },
       },
       {
         id: 5,
-        title: "Working With Data Using Apollo and GraphQL in React Native",
+        title: 'Working With Data Using Apollo and GraphQL in React Native',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "10:15 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '10:15 AM',
         speaker: {
-          name: "Corey Pollock",
+          name: 'Corey Pollock',
           role: "Poor man's Steve Jobs",
-          avatar: "https://api.adorable.io/avatars/285/corey.png"
-        }
-      }
-    ]
+          avatar: 'https://api.adorable.io/avatars/285/corey.png',
+        },
+      },
+    ],
   },
   {
     id: 3456466,
-    time: "11:00 AM",
+    time: '11:00 AM',
     data: [
       {
         id: 6,
-        title: "Styling with the StyleSheet API",
+        title: 'Styling with the StyleSheet API',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "11:00 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '11:00 AM',
         speaker: {
-          name: "Jacob Abraham",
-          role: "Founder & CEO of Jacob By Jacob",
-          avatar: "https://api.adorable.io/avatars/285/jacob.png"
-        }
+          name: 'Jacob Abraham',
+          role: 'Founder & CEO of Jacob By Jacob',
+          avatar: 'https://api.adorable.io/avatars/285/jacob.png',
+        },
       },
       {
         id: 7,
-        title: "Creating Themes with Restyle",
+        title: 'Creating Themes with Restyle',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "11:00 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '11:00 AM',
         speaker: {
-          name: "Matt Legaspi",
-          role: "Figma World Champion",
-          avatar: "https://api.adorable.io/avatars/285/matt.png"
-        }
+          name: 'Matt Legaspi',
+          role: 'Figma World Champion',
+          avatar: 'https://api.adorable.io/avatars/285/matt.png',
+        },
       },
       {
         id: 8,
-        title: "Making Apps Accessible in React Native",
+        title: 'Making Apps Accessible in React Native',
         description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.",
-        time: "11:00 AM",
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris varius nisl sapien, a ullamcorper felis aliquam nec. Aliquam erat volutpat. Donec eget risus pretium orci fermentum aliquam et pellentesque sem. Maecenas luctus dictum odio, imperdiet pharetra felis accumsan quis. Integer lobortis augue felis. Ut non dui gravida, luctus lectus non, semper lectus.',
+        time: '11:00 AM',
         speaker: {
-          name: "Mo Hashi",
-          role: "Director of Business",
-          avatar: "https://api.adorable.io/avatars/285/mo.png"
-        }
-      }
-    ]
+          name: 'Mo Hashi',
+          role: 'Director of Business',
+          avatar: 'https://api.adorable.io/avatars/285/mo.png',
+        },
+      },
+    ],
   },
   {
     id: 456435643,
-    time: "12:00 PM",
+    time: '12:00 PM',
     data: [
       {
         id: 9,
-        title: "Lunch break",
+        title: 'Lunch break',
         description:
-          "Break time! Grab some lunch and coffee and mingle with fellow humans.",
-        time: "12:00 PM",
-        speaker: null
-      }
-    ]
+          'Break time! Grab some lunch and coffee and mingle with fellow humans.',
+        time: '12:00 PM',
+        speaker: null,
+      },
+    ],
   },
   {
     id: 456435643,
-    time: "2:00 PM",
+    time: '2:00 PM',
     data: [
       {
         id: 10,
-        title: "RN Workshop",
+        title: 'RN Workshop',
         description:
-          "Introductory React Native workshop for those looking to get started.",
-        time: "2:00 PM - 5:00 PM",
+          'Introductory React Native workshop for those looking to get started.',
+        time: '2:00 PM - 5:00 PM',
         speaker: {
-          name: "Lloyd Wiredu",
-          role: "React Native Instagram Influencer",
-          avatar: "https://api.adorable.io/avatars/285/lloyd.png"
-        }
-      }
-    ]
-  }
+          name: 'Lloyd Wiredu',
+          role: 'React Native Instagram Influencer',
+          avatar: 'https://api.adorable.io/avatars/285/lloyd.png',
+        },
+      },
+    ],
+  },
 ];
